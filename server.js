fc5c1026cf0a203cc125e8b20dbe208e58fbd784
KhaildHuxley <seokhalidhuxley@gmail.com>
2023-03-24 14:25:34 +0900

    EJS라이브러리 추가, 할 일 목록페이지 추가

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const { response } = require('express');
 const express = require('express');
 //express 라이브러리 첨부
 const app = express();
-
 // const bodyParser = require('body-parser');  
 // app.use(bodyParser.urlencoded({extended : true}));  // <body-parser가 2021년부터 express에 기본적으로 포함되어있음
-
 app.use(express.urlencoded({ extended: true })); // 따라서 원래의 경우엔 사용코드만 적으면 됨
-
-var DB;
 const MongoClient = require('mongodb').MongoClient;
+var DB;
+app.set('view engine', 'ejs');
+
+
 MongoClient.connect('mongodb+srv://seokhalidhuxley:seokhalidhuxley23@cluster0.i8k2gcg.mongodb.net/?retryWrites=true&w=majority'
     , (error, client) => {
         if (error) return console.log(error);
@@ -17,8 +16,9 @@ MongoClient.connect('mongodb+srv://seokhalidhuxley:seokhalidhuxley23@cluster0.i8
 
             app.listen(8080, () => {
                 console.log('listening on 8080')
-            }); //8080 port
+            });
         });
+// mongoDB Atlas > Cluster0 > TODOAPP DB 연결
 
 app.get('/', (req, response) => {
     response.sendFile(__dirname + '/index.html');
@@ -27,6 +27,15 @@ app.get('/', (req, response) => {
 app.get('/write', (req, response) => {
     response.sendFile(__dirname + '/write.html');
 });
+
+app.get('/list', (req, response) => {
+
+    DB.collection('POST').find().toArray((error, result) => {
+         console.log(result);
+         response.render('list.ejs', { TODOs : result});
+        }); // POST collection 내의 모든 데이터꺼내고 list.ejs에 랜더링해서 보여줌
+});
+
 
 app.post('/newpost', (req, response) => {
     response.send('전송완료');
@@ -38,5 +47,6 @@ app.post('/newpost', (req, response) => {
         if (error) return console.log(error);
         console.log('저장완료');
     })
+    // TODOAPP DB > POST collection 연결 > body-parser을 통해 가져온 오브젝트 저장
     
 });
