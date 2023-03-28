2cada241792373ed0e07930117f8ce1adf98ec06
KhaildHuxley <seokhalidhuxley@gmail.com>
2023-03-28 23:38:00 +0900

    삭제요청을 통해 들어온 게시물 DB에서 삭제처리

--- server.js
+++ server.js
@@ -20,39 +20,49 @@ MongoClient.connect('mongodb+srv://seokhalidhuxley:seokhalidhuxley23@cluster0.i8
     });
 // mongoDB Atlas > Cluster0 > TODOAPP DB 연결
 
-app.get('/', (req, response) => {
-    response.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 // __dirname 은 현재경로로 부터라는 뜻
-app.get('/write', (req, response) => {
-    response.sendFile(__dirname + '/write.html');
+app.get('/write', (req, res) => {
+    res.sendFile(__dirname + '/write.html');
 });
 
-app.get('/list', (req, response) => {
+app.get('/list', (req, res) => {
 
     DB.collection('POST').find().toArray((error, result) => {
-        console.log(result);
-        response.render('list.ejs', { TODOs: result });
+        // console.log(result);
+        res.render('list.ejs', { TODOs: result });
     }); // POST collection 내의 모든 데이터꺼내고 list.ejs에 랜더링해서 보여줌
 });
 
 
-app.post('/newpost', (req, response) => {
-    response.send('전송완료');
+app.post('/newpost', (req, res) => {
+    res.send('transfer complete');
     // console.log(req.body) // body-parser를 통해서 form 태그, post요청으로 서버에 들어온 정보를 확인
     // console.log(req.body.title)
     // console.log(req.body.content)
     DB.collection('COUNT').findOne({ name:'postNum'}, (error, result) => {
-        console.log(result.totalPost);
+        // console.log(result.totalPost);
         var postNum = result.totalPost;
 
-        DB.collection('POST').insertOne({ _id: postNum + 1, 제목: req.body.title, 내용: req.body.content },
+        DB.collection('POST').insertOne({ _id: postNum + 1, title: req.body.title, content: req.body.content },
             (error, result) => {
                 if (error) return console.log(error);
-                console.log('저장완료');
+                console.log('save complete');
                 DB.collection('COUNT').updateOne({name:'postNum'}, { $inc: {totalPost: 1}},(error, result) => {
                     if(error){return console.log(error)};
                 });
             });
     });
 });
+
+app.delete('/delete', (req, res) => {
+    // console.log(req.body)
+    req.body._id = parseInt(req.body._id);
+    DB.collection('POST').deleteOne(req.body,(error,result) =>{
+        console.log('delete complete');
+        res.status(200).send();
+    });
+
+})
