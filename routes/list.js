30f9006399f3cc4ff37bd6c92e896432fed4637d
KhaildHuxley <seokhalidhuxley@gmail.com>
2023-04-04 18:23:08 +0900

    Style: Comma 및 test를 위한 세션체크 주석처리
    개발, 테스트 도중 서버 리부팅이 잦아서 편의를 위해 세션체크 주석처리 해뒀음

--- routes/list.js
+++ routes/list.js
@@ -1,20 +1,20 @@
 var router = require('express').Router();
 
 
-function sessionCheck(req, res, next){
-    if (req.user){
-        next()
-    } else {
-        res.send('Only members have access rights')
-    }
-}
-router.use(sessionCheck);
+// function sessionCheck(req, res, next){
+//     if (req.user){
+//         next()
+//     } else {
+//         res.send('Only members have access rights')
+//     }
+// }
+// router.use(sessionCheck);
 
 
 router.get('/list', (req, res) => {
 
     req.app.DB.collection('POST').find().toArray((error, result) => {
-        // console.log(result);
+        console.log(result);
         res.render('list.ejs', { TODOs: result });
     }); // POST collection 내의 모든 데이터꺼내고 list.ejs에 랜더링해서 보여줌
 });
