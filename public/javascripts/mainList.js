f38afc46c582424508e042a4a84b9938ad529e12
KhaildHuxley <seokhalidhuxley@gmail.com>
2023-05-15 23:55:55 +0900

    Feat: 더보기 버튼 기능 구현
    새로 생기는 DOM요소 기능 정상 작동하게 만들기 위해 내부요소들 이벤트 형식으로 코드 리펙토링
    
    페이지 불러올때 모든 데이터 가져오는거에서 최근 글 12개부터 차례도록 불러오도록 코드 리팩토링

--- public/javascripts/mainList.js
+++ public/javascripts/mainList.js
@@ -1,20 +1,26 @@
-// 카드 클릭 시 scale클래스를 가지고 있다면 flipped 클래스 추가
-$(document).ready(function () {
-  $('.card').click(function () {
-    $(this).toggleClass('flipped');
-  });
-  
-  // 내부 요소 클릭 시 이벤트 전파 막기
-  $('.card h4, .card span, .card i').click(function (event) {
-    event.stopPropagation();
-  });
+// seeMore로 인한 신규 DOM요소 생기므로 이벤트 위임을 통해 이벤트 처리
+const eventDelegation = document.querySelector('#main');
+
+// card 클릭 시 flipped 클래스 추가
+$(document).on('click', '.card', function() {
+  $(this).toggleClass('flipped');
 });
 
-$('.card').children().not('h4, span, i').hover(function () {
-  $(this).css('cursor', 'pointer');
-}, function () {
-  $(this).css('cursor', 'auto');
+// 내부 요소 클릭했을때 이벤트 전파 막기
+$(document).on('click', '.card h4, .card span, .card i', function(event) {
+  event.stopPropagation();
 });
+
+
+// 이벤트 위임을 사용하여 hover 이벤트 추가
+$(document).on({
+  mouseenter: function() {
+    $(this).css('cursor', 'pointer');
+  },
+  mouseleave: function() {
+    $(this).css('cursor', 'auto');
+  }
+}, '.card:not(h4, span, i)');
 
 
 // 다른 요소 클릭 시 flipped 클래스 제거
@@ -25,29 +31,37 @@ $(document).on('click', function (e) {
 });
 
 // cardlist hover 시 scale 효과
-$('.cardlist').hover(function () {
-  $(this).css('transform', 'scale(1.05)');
-}, function () {
-  $(this).css('transform', 'scale(1)');
-});
-
-// 카드가 fa-regular 클래스를 가지고 있을땐 아이콘 hover시 fade 효과
-$('.backBtnHeart, .backBtnBookmark').hover(function () {
-  if ($(this).hasClass('fa-regular')) {
-    $(this).addClass('fa-fade');
+$(document).on({
+  mouseenter: function () {
+    $(this).css('transform', 'scale(1.05)');
+  },
+  mouseleave: function () {
+    $(this).css('transform', 'scale(1)');
   }
-}, function () {
-  $(this).removeClass('fa-fade');
-});
+}, '.cardlist');
+
+
+// 이벤트 위임을 사용하여 hover 이벤트 추가
+$(document).on({
+  mouseenter: function() {
+    if ($(this).hasClass('fa-regular')) {
+      $(this).addClass('fa-fade');
+    }
+  },
+  mouseleave: function() {
+    $(this).removeClass('fa-fade');
+  }
+}, '.backBtnHeart, .backBtnBookmark');
+
 
 // 상세페이지 이동
-$('.backBtnComment').on('click', function() {
+$(document).on('click', '.backBtnComment', function() {
   const postId = $(this).data('id');
   window.location.href = `/detail/${postId}`;
 });
 
 // writer class를 가진 div의 첫번째 span을 클릭하면 span의 data-id로 이동하는 이벤트
-$('.writer').find('span').on('click', function() {
+$(document).on('click', '.writer span:first-child', function() {
   const nickname = $(this).data('id');
   window.location.href = `/@${nickname}`;
 });
@@ -242,3 +256,154 @@ backBtnBookmarkList.forEach((backBtnBookmark) => {
     }
   });
 });
+
+// 클라이언트에서 seeMore 버튼 클릭 시 실행되는 함수
+function handleSeeMore() {
+  seeMoreClickCount++;
+  const count = seeMoreClickCount;
+
+  axios.post('/seeMore', {
+    count: count
+  })
+  .then(function (response) {
+    // 받아온 데이터를 이용하여 DOM 요소 생성
+    const newPosts = createDOMElements(response.data);
+
+    // 생성된 DOM 요소를 기존의 게시글 목록에 추가
+    const listDiv = document.querySelector('.listDiv');
+    listDiv.appendChild(newPosts);
+
+    // 받아온 데이터가 12개 미만이면 seeMore 버튼을 숨김
+    if (response.data.POST.length < 12) {
+      seeMoreBtn.style.display = 'none';
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+
+      Toast.fire({
+        icon: 'info',
+        title: '더 이상 게시글이 없습니다'
+      })
+    }
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+// 받아온 데이터를 이용하여 DOM 요소를 생성하는 함수
+function createDOMElements(data) {
+  // DOM 요소를 저장할 컨테이너
+  const fragment = document.createDocumentFragment();
+
+  const sortedPOST = data.POST;
+  const USER = data.USER;
+  const cUSER = data.cUSER;
+  const Luxon = data.Luxon;
+
+  // 데이터를 반복하여 DOM 요소 생성
+  sortedPOST.forEach(sortedPOST => {
+    const postScore = sortedPOST.comment + sortedPOST.like * 2;
+    const transparency = Math.min(1, Math.max(0, postScore / 100)).toFixed(2);
+    const boxShadowColor = `rgba(255, 255, 204, ${transparency})`;
+
+    const currentTime=luxon.DateTime.local().toISO();
+    const postTime=luxon.DateTime.fromISO(sortedPOST.time);
+    const duration=luxon.DateTime.fromISO(currentTime).diff(luxon.DateTime.fromISO(postTime));
+    let timeAgo='';
+    if (duration.as('seconds') < 60) { timeAgo='just before'; } 
+    else if (duration.as('minutes') < 60) { 
+      const minutes = Math.floor(duration.as('minutes'));
+      timeAgo = minutes === 1 ? `${minutes} minute ago` : `${minutes} minutes ago`; 
+    } 
+    else if (duration.as('hours') < 24) {
+      const hours = Math.floor(duration.as('hours'));
+      timeAgo = hours === 1 ? `${hours} hour ago` : `${hours} hours ago`; 
+    } 
+    else if (duration.as('days') < 7) { 
+      const days = Math.floor(duration.as('days'));
+      timeAgo = days === 1 ? `${days} day ago` : `${days} days ago`; 
+    } 
+    else if (duration.as('weeks') < 4) {
+      const weeks = Math.floor(duration.as('weeks'));
+      timeAgo = weeks === 1 ? `${weeks} week ago` : `${weeks} weeks ago`; 
+    } 
+    else if (duration.as('months') < 12) {
+      const months = Math.floor(duration.as('months'));
+      timeAgo = months === 1 ? `${months} month ago` : `${months} months ago`; 
+    } 
+    else {
+      timeAgo = 'long time ago'; 
+    }
+
+    const user = USER.find(u=> u.nickname === sortedPOST.writer);
+
+    const likePosts = cUSER.likePosts.includes(sortedPOST._id);
+    const bookmarkPosts = cUSER.bookmarkPosts.includes(sortedPOST._id);
+
+    const cardlist = document.createElement('div');
+    cardlist.classList.add('cardlist');
+    cardlist.innerHTML = `
+    <div class="card" style="box-shadow: 0px 0px 1vw 1vw ${boxShadowColor}">
+      <div class="front">
+        <div class="frontImg">
+          <img class="frontImg" src=${sortedPOST.img}></img>
+        </div>
+        <div class="frontText">
+          <div class="writingTime">
+            <span class="writeTime">
+              ${timeAgo}
+            </span>
+            <span class="writeTimeHover">
+              ${luxon.DateTime.fromISO(sortedPOST.time).toFormat('yyyy-MM-dd HH:mm')}
+            </span>
+          </div>
+          <div class="frontContent">
+           <h4>
+            ${sortedPOST.title}
+            </h4>
+          </div>
+          <div class="writer">
+          <span data-id="${sortedPOST.writer}"> 
+            ${sortedPOST.writer}
+          </span>
+           <img src="${user.profileImg}"></img>
+          </div>
+        </div>
+      </div>
+      <div class="back">
+        <div class="backContent">
+         <span>
+          ${sortedPOST.content}
+          </span>
+        </div>
+        <div class="backBtnBundle">
+          <i class="fa-regular fa-comment-dots backBtnComment" data-id="${sortedPOST._id}" style="color: #ececec;"></i>
+          <i class="${likePosts ? `fa-solid` : `fa-regular`} fa-heart backBtnHeart" data-id="${sortedPOST._id}" style="color: ${likePosts ? `rgba(255, 0, 0, .5)` : '#ececec'};"></i>
+          <i class="${bookmarkPosts ? `fa-solid` : `fa-regular`} fa-bookmark backBtnBookmark" data-id="${sortedPOST._id}" style="color: #ececec;"></i>
+        </div>
+      </div>
+    </div>
+ `;
+
+    // DOM 요소를 fragment에 추가
+    fragment.appendChild(cardlist);
+  });
+
+  // fragment를 반환
+  return fragment;
+}
+
+// seeMore 버튼에 클릭 이벤트 리스너 추가
+const seeMoreBtn = document.getElementById('seeMore');
+let seeMoreClickCount = 0;
+seeMoreBtn.addEventListener('click', handleSeeMore);
--- routes/main.js
+++ routes/main.js
@@ -3,19 +3,43 @@ const luxon = require('luxon');
 
 // 메인 페이지
 router.get('/main', async (req, res) => {
-    try{
-        const [postResult, userResult] = await Promise.all([
-            req.app.DB.collection('POST').find().toArray(),
-            req.app.DB.collection('USER').find().toArray()
-          ]);
+    try {
 
-          //로그인 유저 정보
-          const currentUserResult = userResult.find(user => user.id === req.user.id);
+      const postResult = await req.app.DB.collection('POST').find({ isDeleted: false }).sort({ time: -1 }).limit(12).toArray();
+  
+      const authorIds = Array.from(new Set(postResult.map(post => post.writer)));
+  
+      const [authorsResult, currentUserResult] = await Promise.all([
+        req.app.DB.collection('USER').find({ nickname: { $in: authorIds } }).toArray(),
+        req.app.DB.collection('USER').findOne({ id: req.user.id })
+      ]);
 
-          res.render('main.ejs', { POST: postResult, USER: userResult, cUSER: currentUserResult, luxon: luxon });
+      res.render('main.ejs', { POST: postResult, USER: authorsResult, cUSER: currentUserResult, luxon: luxon });
+    } catch (error) {
+      res.status(400).send('400 Bad Request');
+    }
+});
+
+// 메인 페이지 더보기
+router.post('/seeMore', async (req, res) => {
+    try {
+
+        const seeMoreCount = req.body.count;
+
+        const postResult = await req.app.DB.collection('POST').find({ isDeleted: false }).sort({ time: -1 }).skip(12 * seeMoreCount).limit(12).toArray();
+
+        const authorIds = Array.from(new Set(postResult.map(post => post.writer)));
+
+        const [authorsResult, currentUserResult] = await Promise.all([
+            req.app.DB.collection('USER').find({ nickname: { $in: authorIds } }).toArray(),
+            req.app.DB.collection('USER').findOne({ id: req.user.id })
+        ]);
+
+        res.json({ POST: postResult, USER: authorsResult, cUSER: currentUserResult, luxon: luxon });
     } catch (error) {
         res.status(400).send('400 Bad Request');
     }
 });
+        
 
 module.exports = router;
